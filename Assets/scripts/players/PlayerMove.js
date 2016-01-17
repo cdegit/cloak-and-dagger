1815ba9f563c792d9cbf6c5661032c60815a54a8
Cassandra de Git <cassandra.m.degit@gmail.com>
2016-01-16 22:58:07 -0800

    Basic networking

---
+++ Assets/scripts/players/PlayerMove.js
@@ -0,0 +1,23 @@
+﻿#pragma strict
+
+public class PlayerMove extends UnityEngine.Networking.NetworkBehaviour {
+
+    private var speedModifier = 0.4;
+
+    function Update() {
+
+        if (!isLocalPlayer) {
+            return;
+        }
+
+        var verticalSpeed = Input.GetAxis("Vertical");
+        var horizontalSpeed = Input.GetAxis("Horizontal");
+
+        var newPosition = transform.position;
+
+        newPosition.x += horizontalSpeed * speedModifier;
+        newPosition.y += verticalSpeed * speedModifier;
+
+        transform.position = newPosition;
+    }
+}
