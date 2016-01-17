48bd505e36bb6d54f8d5c11179921f0f82fd6e4b
Cassandra de Git <cassandra.m.degit@gmail.com>
2016-01-16 23:09:03 -0800

    Add basic networked NPCs

---
+++ Assets/scripts/crowds/CrowdSpawner.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class CrowdSpawner : NetworkBehaviour {
+
+    public GameObject NPCPrefab;
+    public int numNPCs;
+
+    public override void OnStartServer() {
+        for (int i = 0; i < numNPCs; i++)
+        {
+            var pos = new Vector3(
+                Random.Range(-8.0f, 8.0f),
+                0.2f,
+                Random.Range(-8.0f, 8.0f)
+            );
+            var rotation = Quaternion.Euler(0, 0, 0);
+            var npc = (GameObject)Instantiate(NPCPrefab, pos, rotation);
+
+            NetworkServer.Spawn(npc);
+        }
+    }
+}
