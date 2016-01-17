e863953fb7ec05e103e954a82327798468fbac1e
Cassandra de Git <cassandra.m.degit@gmail.com>
2016-01-16 21:23:46 -0800

    Set up isometric environment

---
+++ Assets/scripts/players/movement.js
@@ -0,0 +1,15 @@
+﻿#pragma strict
+
+var speedModifier = 0.5;
+
+function FixedUpdate () {
+    var verticalSpeed = Input.GetAxis("Vertical");
+    var horizontalSpeed = Input.GetAxis("Horizontal");
+
+    var newPosition = transform.position;
+
+    newPosition.x += horizontalSpeed;
+    newPosition.y += verticalSpeed;
+
+    transform.position = newPosition;
+}
