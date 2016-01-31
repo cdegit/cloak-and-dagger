a20f470879d7e6124b127f07d11c6c2b22c83fab
Cassandra de Git <cassandra.m.degit@gmail.com>
2016-01-30 17:00:37 -0800

    Added class to handle object interactions

---
+++ Assets/scripts/interactions/basket.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class basket : interactInRange {
+
+    public override void SeekerInteraction() {
+        Debug.Log("Seekers's interaction with basket");
+    }
+
+    public override void HunterInteraction() {
+        Debug.Log("Hunter's interaction with basket");
+    }
+}
---
+++ Assets/scripts/interactions/interactInRange.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// This script requires that the game object it is attached to has:
+//     a Renderer
+//     a 2D Collider set to trigger
+
+public class interactInRange : MonoBehaviour {
+    private bool withinRange = false;
+
+    void Update() {
+        if (withinRange) {
+            // TODO: Check player type
+            if (Input.GetButtonUp("Fire1")) {
+                this.SeekerInteraction();
+            }
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other) {
+        // highlight this object
+        // show a GUI instruction (maybe)
+        withinRange = true;
+        GetComponent<Renderer>().material.color = Color.red;
+    }
+
+    void OnTriggerExit2D(Collider2D other) {
+        withinRange = false;
+        GetComponent<Renderer>().material.color = Color.white;
+    }
+
+    public virtual void SeekerInteraction() {
+
+    }
+
+    public virtual void HunterInteraction() {
+
+    }
+}
