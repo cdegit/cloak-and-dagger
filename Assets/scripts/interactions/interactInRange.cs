46c0bd577a1b5347b4be8366729cd3b9490b85c8
Cassandra de Git <cassandra.m.degit@gmail.com>
2016-02-07 16:22:13 -0800

    Set the hosting player as the hunter

--- Assets/scripts/interactions/interactInRange.cs
+++ Assets/scripts/interactions/interactInRange.cs
@@ -6,27 +6,26 @@ using System.Collections;
 //     a 2D Collider set to trigger
 
 public class interactInRange : MonoBehaviour {
-    private bool withinRange = false;
-
-    void Update() {
-        if (withinRange) {
-            // TODO: Check player type
-            if (Input.GetButtonUp("Fire1")) {
-                this.SeekerInteraction();
-            }
-        }
-    }
-
     void OnTriggerEnter2D(Collider2D other) {
         // highlight this object
         // show a GUI instruction (maybe)
-        withinRange = true;
         GetComponent<Renderer>().material.color = Color.red;
     }
 
     void OnTriggerExit2D(Collider2D other) {
-        withinRange = false;
         GetComponent<Renderer>().material.color = Color.white;
+    }
+
+    // Note: For this to work, the rigidbody in the player must be set to never sleep
+    // Otherwise this just isn't fired
+    void OnTriggerStay2D (Collider2D other) {
+        if (other.gameObject.tag == "Player" && Input.GetButtonUp("Fire1")) {
+            if (other.gameObject.GetComponent<PlayerIdentity>().IsHunter()) {
+                this.HunterInteraction();
+            } else {
+                this.SeekerInteraction();
+            }
+        }
     }
 
     public virtual void SeekerInteraction() {
---
+++ Assets/scripts/networking/PlayerIdentity.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerIdentity : UnityEngine.Networking.NetworkBehaviour {
+
+    private Renderer localRenderer;
+    private bool isHunter;
+
+    void Start () {
+        localRenderer = GetComponent<Renderer>();
+
+        // Until we allow the players to select their character:
+        // If this is the host, set the localPlayer to the Hunter and the other player to the Seeker
+        // If this is the client, set the localPlayer to the Seeker and the other player to the Hunter
+        // TL;DR If you host, you play as the Hunter
+        if (isServer) {
+            if (isLocalPlayer) {
+                InitAsHunter();
+            } else {
+                InitAsSeeker();
+            }
+        } else {
+            if (isLocalPlayer) {
+                InitAsSeeker();
+            } else {
+                InitAsHunter();
+            }
+        }
+    }
+
+    void InitAsHunter () {
+        localRenderer.material.color = Color.red;
+        isHunter = true;
+    }
+
+    void InitAsSeeker () {
+        localRenderer.material.color = Color.blue;
+        isHunter = false;
+    }
+
+    public bool IsHunter () {
+        return isHunter;
+    }
+
+    public bool IsSeeker () {
+        return !isHunter;
+    }
+}
