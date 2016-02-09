1bdb56f60c06ed748eb9717bea88d59e953b7be9
Cassandra de Git <cassandra.m.degit@gmail.com>
2016-02-08 23:19:05 -0800

    NPCs can now move between waypoints

--- Assets/scripts/crowds/NPCMovement.cs
+++ Assets/scripts/crowds/NPCMovement.cs
@@ -1,18 +1,45 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+// Based on the code provided by Emily in the sample stealth project
 public class NPCMovement : MonoBehaviour {
+    public Transform[] waypoints;
 
-    public Transform target;
     private NavMeshAgent navAgent;
 
-	void Start () {
+    int waypointIndex = 0;
+
+    float minDistance = 2f;
+    float patrolWaitTime = 2;
+    float patrolWaitTimer;
+
+    void Start () {
         navAgent = GetComponent<NavMeshAgent>();
-	}
+
+        // Disable rotation, so that the sprite is always facing the camera
+        // ... Once the sprite is in place, of course
+        navAgent.updateRotation = false;
+
+        // Start moving to the first waypoint
+        navAgent.SetDestination(waypoints[waypointIndex].position);
+    }
 	
 	void Update () {
-	    if (target) {
-            navAgent.SetDestination(target.transform.position);
+        if (ReferenceEquals(navAgent.destination, null) || navAgent.remainingDistance < minDistance) {
+            //increment by Time.deltaTime because this increments wrt time (in seconds)
+            patrolWaitTimer += Time.deltaTime;
+
+            if (patrolWaitTimer >= patrolWaitTime) {
+                if (waypointIndex >= waypoints.Length - 1) {
+                    waypointIndex = 0;
+                } else {
+                    waypointIndex++;
+                }
+                
+                navAgent.SetDestination(waypoints[waypointIndex].position);
+
+                patrolWaitTimer = 0;
+            }
         }
-	}
+    }
 }
