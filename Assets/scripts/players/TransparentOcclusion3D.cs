d1d9ba09069aa4aad14ed2a182e10e0b33543263
Cassandra de Git <cassandra.m.degit@gmail.com>
2016-02-08 22:08:37 -0800

    Make transparent occlusion stuff work in 3D

---
+++ Assets/scripts/players/TransparentOcclusion3D.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TransparentOcclusion3D : MonoBehaviour {
+
+	void FixedUpdate () {
+        // Send a ray from the camera to the player
+        // If the ray hits something that can occlude the player along the way, try to activate its Transparent Occlusion Handler
+        // This will make the other object fade to 50% opacity
+        RaycastHit hitInfo;
+
+        if (Physics.Linecast(Camera.main.transform.position, transform.position, out hitInfo)) {
+            // Currently the object needs to have both the tag and the script
+            // The script alone may be sufficient
+            if (hitInfo.collider.CompareTag("Occludable")) {
+                TransparentOcclusionHandler occludingObjectHandler = hitInfo.collider.gameObject.GetComponent<TransparentOcclusionHandler>();
+
+                if (occludingObjectHandler) {
+                    occludingObjectHandler.BecomeTranslucent();
+                }
+            }
+        }
+    }
+}
---
+++ Assets/scripts/players/TransparentOcclusionHandler.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TransparentOcclusionHandler : MonoBehaviour {
+    private float duration = 0.5f;
+    private float t = 0f;
+
+    private bool occludingPlayerThisFrame = false;
+
+    private Renderer localRenderer;
+    private Color originalColor;
+
+    void Start() {
+        localRenderer = GetComponent<Renderer>();
+        originalColor = localRenderer.material.color;
+    }
+
+    void Update() {
+        // Inspired by http://answers.unity3d.com/questions/328891/controlling-duration-of-colorlerp-in-seconds.html
+        localRenderer.material.color = Color.Lerp(originalColor, new Color(originalColor.r, originalColor.g, originalColor.b, 0.5f), t);
+
+        if (occludingPlayerThisFrame) {
+            // This will make the color more transparent over time
+            if (t < 1) {
+                t += Time.deltaTime / duration;
+            }
+        } else {
+            // This will make the color more opaque over time
+            if (t > 0) {
+                t -= Time.deltaTime / duration;
+            }
+        }
+
+        occludingPlayerThisFrame = false;
+    }
+
+    public void BecomeTranslucent() {
+        occludingPlayerThisFrame = true;
+    }
+}
