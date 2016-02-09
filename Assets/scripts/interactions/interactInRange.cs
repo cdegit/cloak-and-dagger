ddb98c8320d33e69288e6e18524cde96009eb9d4
Cassandra de Git <cassandra.m.degit@gmail.com>
2016-02-08 22:23:28 -0800

    Make items work in 3D

--- Assets/scripts/interactions/interactInRange.cs
+++ Assets/scripts/interactions/interactInRange.cs
@@ -3,22 +3,22 @@ using System.Collections;
 
 // This script requires that the game object it is attached to has:
 //     a Renderer
-//     a 2D Collider set to trigger
+//     a Collider set to trigger
 
 public class interactInRange : MonoBehaviour {
-    void OnTriggerEnter2D(Collider2D other) {
+    void OnTriggerEnter(Collider other) {
         // highlight this object
         // show a GUI instruction (maybe)
         GetComponent<Renderer>().material.color = Color.red;
     }
 
-    void OnTriggerExit2D(Collider2D other) {
+    void OnTriggerExit(Collider other) {
         GetComponent<Renderer>().material.color = Color.white;
     }
 
     // Note: For this to work, the rigidbody in the player must be set to never sleep
     // Otherwise this just isn't fired
-    void OnTriggerStay2D (Collider2D other) {
+    void OnTriggerStay (Collider other) {
         if (other.gameObject.tag == "Player" && Input.GetButtonUp("Fire1")) {
             if (other.gameObject.GetComponent<PlayerIdentity>().IsHunter()) {
                 this.HunterInteraction();
