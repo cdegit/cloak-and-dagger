acf64338d40cd3e99abf887769986baea3bbad27
Cassandra de Git <cassandra.m.degit@gmail.com>
2016-02-09 00:06:55 -0800

    Added seeker victory scene

---
+++ Assets/scripts/interactions/Podium.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Podium : MonoBehaviour {
+    public GameObject gameEndManager;
+
+    void OnTriggerEnter(Collider other) {
+        if (other.gameObject.tag == "Player" && other.gameObject.GetComponent<PlayerIdentity>().IsSeeker()) {
+            // Tell the server to tell both players that the seeker has won
+            gameEndManager.GetComponent<GameEndManager>().RpcSeekerVictory();
+        }
+    }
+}
---
+++ Assets/scripts/networking/GameEndManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+using UnityEngine.SceneManagement;
+
+public class GameEndManager : UnityEngine.Networking.NetworkBehaviour {
+
+    // Do other objects make requests to this object for the game to end?
+    // Does this object check if the game end conditions have been met? that'd be silly
+
+    [ClientRpc]
+    public void RpcSeekerVictory() {
+        SceneManager.LoadScene("seekerVictory");
+    }
+
+    [ClientRpc]
+    void RpcHunterVictory() {
+        SceneManager.LoadScene("hunterVictory");
+    }
+}
