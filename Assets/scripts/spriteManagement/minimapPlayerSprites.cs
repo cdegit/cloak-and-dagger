dc2b90cebb1b1122859245c8aeef9a3ae05693cd
Cassandra de Git <cassandra.m.degit@gmail.com>
2016-02-27 23:39:07 -0800

    Add minimap icons for the Hunter and Seeker

---
+++ Assets/scripts/spriteManagement/minimapPlayerSprites.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Set the correct icon depending on this player's type
+public class minimapPlayerSprites : MonoBehaviour {
+	public Sprite hunterSprite;
+	public Sprite seekerSprite;
+
+	void Start() {
+		PlayerIdentity id = transform.parent.GetComponent<PlayerIdentity>();
+		SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
+
+		if (id.IsHunter()) {
+			spriteRenderer.sprite = hunterSprite;
+		} else {
+			spriteRenderer.sprite = seekerSprite;
+		}
+	}
+}
