03dad12db1fce3dbe459d9947ef1848b3ffae68b
Cassandra de Git <cassandra.m.degit@gmail.com>
2016-02-27 23:10:26 -0800

    Disable shadows on minimap

---
+++ Assets/scripts/disableShadows.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// From http://answers.unity3d.com/answers/13187/view.html
+public class disableShadows : MonoBehaviour {
+	private float storedShadowDistance;
+
+	void OnPreRender() {
+		storedShadowDistance = QualitySettings.shadowDistance;
+		QualitySettings.shadowDistance = 0;
+	}
+
+	void OnPostRender() {
+		QualitySettings.shadowDistance = storedShadowDistance;
+	}
+}
