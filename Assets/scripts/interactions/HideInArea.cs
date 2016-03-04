65a8bf1c8b59d69e1c6074603335546d898c3f9a
Cassandra de Git <cassandra.m.degit@gmail.com>
2016-03-03 22:42:25 -0800

    Added basic water

---
+++ Assets/scripts/interactions/HideInArea.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HideInArea : interactInRange {
+    public override void SeekerInteraction(Collider thisPlayer) {
+        HidingManager hidingManager = thisPlayer.gameObject.GetComponent<HidingManager>();
+
+        if (!hidingManager.IsHiding()) {
+            hidingManager.CmdHideInArea();
+        } else {
+            hidingManager.CmdStopHiding();
+        }
+    }
+
+    public override void HunterInteraction(Collider thisPlayer) {
+        // Na da
+    }
+
+	public override void OnTriggerExit(Collider thisPlayer) {
+		base.OnTriggerExit(thisPlayer);
+        // Leave grass when the player walks outside
+        HidingManager hidingManager = thisPlayer.gameObject.GetComponent<HidingManager>();
+        hidingManager.CmdStopHiding();
+    }
+}
