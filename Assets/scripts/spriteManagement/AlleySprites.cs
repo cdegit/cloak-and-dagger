ad23256c47a2118af44751f5e349f7df81c9c20d
Cassandra de Git <cassandra.m.degit@gmail.com>
2016-03-04 16:31:24 -0800

    Added alley sprites

---
+++ Assets/scripts/spriteManagement/AlleySprites.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AlleySprites : MonoBehaviour {
+	private Animator animator;
+	private GameObject otherEnd;
+
+	void Start () {
+		animator = GetComponent<Animator>();
+		otherEnd = GetComponent<Alley>().otherEnd;
+
+		Vector3 offset = otherEnd.transform.position - transform.position;
+
+		float angle = Vector3.Angle(offset, transform.right);
+
+		if (offset.z < 0) {
+			angle = 360 - angle;
+		}
+
+		animator.SetFloat("angle", angle);
+	}
+}
