4dfb71a5827475b0f425292cc42819bf6733d80e
Cassandra de Git <cassandra.m.degit@gmail.com>
2016-03-28 21:01:37 -0700

    Add progress bar to gate

--- Assets/scripts/interactions/Gate.cs
+++ Assets/scripts/interactions/Gate.cs
@@ -11,18 +11,23 @@ public class Gate : interactInRange {
 	private float unlockTime = 1f; // seconds
 	private float unlockTimer = 0;
 
+	private InWorldUI ui;
+
 	void Start() {
 		obstacle = GetComponent<NavMeshObstacle>();
+		ui = GetComponentInChildren<InWorldUI>();
 	}
 
 	public override void Update() {
 		base.Update();
 
 		if (unlockTimer < unlockTime && unlocking) {
+			ui.SetProgress((unlockTimer/unlockTime) * 100);
 			unlockTimer += Time.deltaTime;
 		}
 
 		if (unlockTimer > unlockTime) {
+			ui.SetProgress(100);
 			player.gameObject.GetComponent<GatePlayer>().UnlockGate(gameObject);
 			unlockTimer = unlockTime;
 		}
--- Assets/scripts/ui/InWorldUI.cs
+++ Assets/scripts/ui/InWorldUI.cs
@@ -1,17 +1,32 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class InWorldUI : MonoBehaviour {
+
+	public bool onlyShowWhenChanging = false;
+
 	private float progress = 100;
 	private RectTransform rect;
+	private Image img;
+	private Color originalColor;
 
 	void Start() {
 		rect = GetComponent<RectTransform>();
+		// img is the mask image
+		img = transform.parent.gameObject.GetComponent<Image>();
+		originalColor = img.color;
 	}
 
 	void Update() {
 		if (!System.Single.IsInfinity(progress)) {
 			rect.anchoredPosition = new Vector2(-1 + (progress / 100), 0);
+		}
+
+		if (onlyShowWhenChanging && (progress == 0 || progress == 100)) {
+			img.color = new Color(0, 0, 0, 0);
+		} else {
+			img.color = originalColor;
 		}
 	}
 
