42091a4ac6d4106c1e06cd53b26632c3967c6aff
Cassandra de Git <cassandra.m.degit@gmail.com>
2016-04-02 23:17:49 -0700

    Add audio to the title screen

---
+++ Assets/scripts/DontDestroy.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// http://answers.unity3d.com/questions/11314/audio-or-music-to-continue-playing-between-scene-c.html
+public class DontDestroy : MonoBehaviour {
+	private static DontDestroy instance = null;
+
+	public static DontDestroy Instance {
+		get {
+			return instance;
+		}
+	}
+
+	void Awake() {
+		if (instance != null && instance != this) {
+			Destroy(this.gameObject);
+			return;
+		} else {
+			instance = this;
+		}
+		DontDestroyOnLoad(this.gameObject);
+	}
+}
