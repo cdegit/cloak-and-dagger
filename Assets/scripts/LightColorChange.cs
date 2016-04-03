b7cf05b4f76a7ee38ddfc59a9524fc5a979c806b
Cassandra de Git <cassandra.m.degit@gmail.com>
2016-04-03 15:25:06 -0700

    Add a gradual lighting change

---
+++ Assets/scripts/LightColorChange.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightColorChange : MonoBehaviour {
+	private Light lt;
+	public Color startColor;
+	public Color endColor;
+	public float duration;
+
+	void Start () {
+		lt = GetComponent<Light>();
+		lt.color = startColor;
+	}
+
+	void Update () {
+		lt.color = Color.Lerp(startColor, endColor, Time.time / duration);
+	}
+}
