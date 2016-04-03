e59fbac3c52eaee2b30f5f7c89a546c1b46ab48f
Cassandra de Git <cassandra.m.degit@gmail.com>
2016-04-02 23:49:07 -0700

    Added music manager

---
+++ Assets/scripts/MusicManager.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicManager : MonoBehaviour {
+	public AudioSource titleScreen;
+	public AudioSource garden;
+
+	public float crossFadeModifier = 0.5f;
+
+	private AudioSource currentAudio;
+	private AudioSource newAudio;
+
+	private bool doCrossfade;
+
+
+	void Start() {
+		currentAudio = titleScreen;
+	}
+
+	void Update() {
+		if (doCrossfade) {
+			Crossfade();
+		}
+	}
+
+	void Crossfade() {
+		if (!currentAudio || !newAudio) {
+			return;
+		}
+
+		if (!newAudio.isPlaying) {
+			newAudio.Play();
+		}
+
+		if (currentAudio.volume > 0.01) {
+			currentAudio.volume -= crossFadeModifier * Time.deltaTime;
+		} else if (currentAudio.isPlaying) {
+			currentAudio.Stop();
+		}
+
+		if (newAudio.volume < 1) {
+			newAudio.volume +=  crossFadeModifier * Time.deltaTime;
+		} else {
+			currentAudio = newAudio;
+			newAudio = null;
+			doCrossfade = false;
+		}
+	}
+
+	public void PlayGardenMusic() {
+		doCrossfade = true;
+		newAudio = garden;
+	}
+}
---
+++ Assets/scripts/Region.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Region : MonoBehaviour {
+	public enum RegionEnum{Desert, City, Garden};
+	public RegionEnum region;
+
+	private MusicManager music;
+
+	void Start() {
+		music = GameObject.Find("Music").GetComponent<MusicManager>();
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (!music) {
+			return;
+		}
+
+		if (region == RegionEnum.Garden) {
+			music.PlayGardenMusic();
+		}
+	}
+}
